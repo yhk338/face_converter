ecd6a54ac6f9857324b1104459c528d5676b3ff8
yhk338 <yanghwi42@gmail.com>
2024-04-27 14:56:01 -0700

    updated client

---
+++ client/src/components/ImageOutput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ImageOutput() {
+  return (
+    <div>
+      <img src="#" alt="Processed Image" /> {/* Placeholder image */}
+    </div>
+  );
+}
+
+export default ImageOutput;
