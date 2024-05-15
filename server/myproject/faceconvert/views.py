34499d0f982e5303f39aa3f483daf1d2fedd5ba4
yhk338 <yanghwi42@gmail.com>
2024-05-14 22:21:23 -0700

    created django

---
+++ server/myproject/faceconvert/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+import cv2
+import matplotlib.pyplot as plt
+from deepface import DeepFace
+import numpy as np
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import os
+
+@csrf_exempt
+def process_image(request):
+    if request.method == 'POST' and request.FILES['image']:
+        image_file = request.FILES['image']
+        image_path = f'media/{image_file.name}'
+        
+        with open(image_path, 'wb+') as destination:
+            for chunk in image_file.chunks():
+                destination.write(chunk)
+        
+        # Load the models
+        age_model = cv2.dnn.readNetFromCaffe("age.prototxt", "dex_chalearn_iccv2015.caffemodel")
+        gender_model = cv2.dnn.readNetFromCaffe("gender.prototxt", "gender.caffemodel")
+        
+        # Read and process the image
+        img = cv2.imread(image_path)
+        haar_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        
+        def detect_faces(img):
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = haar_detector.detectMultiScale(gray, 1.3, 5)
+            return faces
+        
+        faces = detect_faces(img)
+        for x, y, w, h in faces:
+            detected_face = img[int(y):int(y+h), int(x):int(x+w)]
+        
+        detected_face = cv2.resize(detected_face, (224, 224))
+        img_blob = cv2.dnn.blobFromImage(detected_face)
+        
+        age_model.setInput(img_blob)
+        age_dist = age_model.forward()[0]
+        
+        gender_model.setInput(img_blob)
+        gender_class = gender_model.forward()[0]
+        
+        output_indexes = np.array([i for i in range(0, 101)])
+        apparent_predictions = round(np.sum(age_dist * output_indexes), 2)
+        
+        gender = 'Woman ' if np.argmax(gender_class) == 0 else 'Man'
+        
+        demography = DeepFace.analyze(image_path, actions = ['age', 'gender'])
+        age = demography[0]["age"]
+        ret_gender = 'Woman' if demography[0]["gender"]['Woman'] > demography[0]["gender"]['Man'] else 'Man'
+        
+        # Clean up the uploaded image
+        os.remove(image_path)
+        
+        return JsonResponse({
+            'age': age,
+            'gender': ret_gender,
+            'demography': demography[0]["gender"]
+        })
+    
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+def upload_image(request):
+    return render(request, 'upload.html')
